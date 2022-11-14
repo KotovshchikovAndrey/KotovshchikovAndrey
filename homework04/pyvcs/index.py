159e011415809e5b06b0e9d173d9b74c1b679aff
KotovshchikovAndrey <ykt_andrey@mail.ru>
2022-11-14 03:12:44 +0300

    Complete index update cli functions

--- homework04/pyvcs/index.py
+++ homework04/pyvcs/index.py
@@ -1,3 +1,4 @@
+import time
 import hashlib
 import operator
 import os
@@ -16,11 +17,11 @@ UNPACK_FORMAT = ">2i8i20sh{name_length}s"
 # Размер всех атрибутов за исключением name известен, и в сумме составляет 62 байта
 DEFINITE_PACK_SIZE = 62
 
-# Байты в начале и конце файла index
-INDEX_START_BYTES, INDEX_END_BYTES = (
-    b"DIRC\x00\x00\x00\x02\x00\x00\x00\x03",
-    b"k\xd6q\xa7d\x10\x8e\x80\x93F]\x0c}+\x82\xfb\xc7:\xa8\x11"
-)
+# # Байты в начале и конце файла index
+# INDEX_START_BYTES, INDEX_END_BYTES = (
+#     b"DIRC\x00\x00\x00\x02\x00\x00\x00\x03",
+#     b"k\xd6q\xa7d\x10\x8e\x80\x93F]\x0c}+\x82\xfb\xc7:\xa8\x11"
+# )
 
 
 class GitIndexEntry(tp.NamedTuple):
@@ -60,29 +61,47 @@ def read_index(gitdir: pathlib.Path) -> tp.List[GitIndexEntry]:
     if not index_path.exists():
         return []
 
-    index_data = index_path.read_bytes()
-    read_start, read_end = (
-        len(INDEX_START_BYTES) + 1,
-        len(index_data) - len(INDEX_END_BYTES)
-    )
-    pack_files_list = index_data[read_start:read_end].split(b"\x00\x00\x00^")
+    readed_data = index_path.read_bytes()
+    index_data = readed_data[12:len(readed_data) - 20]
 
-    return [GitIndexEntry.unpack(b"^" + pack_data) for pack_data in pack_files_list]
+    contant_range = 0
+    split_data_list = []
+    while contant_range < len(index_data):
+        start_read_content = contant_range + DEFINITE_PACK_SIZE
+        separator_index = index_data[start_read_content:].find(b'\x00\x00\x00')
+        next_data_index = start_read_content + separator_index + 3
+        split_data_list.append(
+            index_data[contant_range:next_data_index])
+
+        contant_range = next_data_index
+
+    return sorted(
+        [GitIndexEntry.unpack(pack_data) for pack_data in split_data_list],
+        key=lambda entry: entry.name
+    )
 
 
 def write_index(gitdir: pathlib.Path, entries: tp.List[GitIndexEntry]) -> None:
-    index_data_for_write = INDEX_START_BYTES
+    index_data_for_write = struct.pack(">4s2i", b"DIRC", 2, len(entries))
     for index_entry in entries:
         index_data_for_write += index_entry.pack()
 
+    hash_sum = hashlib.sha1(index_data_for_write).hexdigest()
+    sha = bytes(bytearray.fromhex(hash_sum))
+
     index_path = gitdir / 'index'
-    index_path.write_bytes(
-        index_data_for_write + INDEX_END_BYTES)
+    index_path.write_bytes(index_data_for_write + sha)
 
 
 def ls_files(gitdir: pathlib.Path, details: bool = False) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    index_data = read_index(gitdir)
+    if not details:
+        print(*[entry.name for entry in index_data], sep="\n")
+    else:
+        print(
+            *[f"100{oct(entry.mode)[-3:]} {entry.sha1.hex()} 0	{entry.name}" for entry in index_data],
+            sep="\n"
+        )
 
 
 def update_index(gitdir: pathlib.Path, paths: tp.List[pathlib.Path], write: bool = True) -> None:
@@ -107,9 +126,9 @@ def update_index(gitdir: pathlib.Path, paths: tp.List[pathlib.Path], write: bool
             uid=stat_info.st_uid,
             gid=stat_info.st_gid,
             size=stat_info.st_size,
-            sha1=file_hash.encode(),
+            sha1=bytes(bytearray.fromhex(file_hash)),
             flags=7,
-            name=file.name
+            name=str(file).replace('\\', '/')
         )
 
         git_index_entry_list.append(git_index_entry)
