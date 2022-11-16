a45125e96222a96eeff026da287f3129aa56f082
KotovshchikovAndrey <ykt_andrey@mail.ru>
2022-11-17 02:08:24 +0300

    Create Session and get_freands func

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,7 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.DOtJ6Ciev2zZz5WSF7orxEvjUjbOPO_BdDBI754YRtLBwAcK3dUhYejudCiUdB_6sytxNkGEXy4Hj68XgEps0JTrBvKcziC-7ARaxHqvnBCkzUHy4z3fxowIxIKXD_HtRxI_fUZKXdXYiwwhqt7_ERvmXxBAxT-Xxqg_XbjQtY3EQFRbVPnUd8s8WVpbKpW0",
     "version": "5.126",
+    "client_id": 51478524
 }
--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -2,9 +2,11 @@ import dataclasses
 import math
 import time
 import typing as tp
+import requests
 
 from vkapi import config, session
 from vkapi.exceptions import APIError
+from vkapi.config import VK_CONFIG
 
 QueryParams = tp.Optional[tp.Dict[str, tp.Union[str, int]]]
 
@@ -18,17 +20,21 @@ class FriendsResponse:
 def get_friends(
     user_id: int, count: int = 5000, offset: int = 0, fields: tp.Optional[tp.List[str]] = None
 ) -> FriendsResponse:
-    """
-    Получить список идентификаторов друзей пользователя или расширенную информацию
-    о друзьях пользователя (при использовании параметра fields).
+    query_params = {
+        'user_id': user_id,
+        'count': count,
+        'offset': offset,
+        'fields': fields
+    }
 
-    :param user_id: Идентификатор пользователя, список друзей для которого нужно получить.
-    :param count: Количество друзей, которое нужно вернуть.
-    :param offset: Смещение, необходимое для выборки определенного подмножества друзей.
-    :param fields: Список полей, которые нужно получить для каждого пользователя.
-    :return: Список идентификаторов друзей пользователя или список пользователей.
-    """
-    pass
+    response = requests.get(
+        url=f"{VK_CONFIG.get('domain')}/friends.get?access_token={VK_CONFIG.get('access_token', '')}",
+        params=query_params
+    )
+
+    if response.status_code == 200:
+        response_data = response.json()['response']
+        return FriendsResponse(**response_data)
 
 
 class MutualFriends(tp.TypedDict):
