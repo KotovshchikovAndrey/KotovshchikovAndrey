9241d2e7aa5f71afc8e296068a507c47c8ab03ac
KotovshchikovAndrey <ykt_andrey@mail.ru>
2022-11-21 16:33:31 +0300

    remove token

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,7 +2,7 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "vk1.a.DOtJ6Ciev2zZz5WSF7orxEvjUjbOPO_BdDBI754YRtLBwAcK3dUhYejudCiUdB_6sytxNkGEXy4Hj68XgEps0JTrBvKcziC-7ARaxHqvnBCkzUHy4z3fxowIxIKXD_HtRxI_fUZKXdXYiwwhqt7_ERvmXxBAxT-Xxqg_XbjQtY3EQFRbVPnUd8s8WVpbKpW0",
+    "access_token": "ХРЕН ВАМ",
     "version": "5.126",
     "client_id": 51478524,
     "target_limit": 100
